c2ab00e6013ab04432c2ee69f264b8bf1defb037
fero <fero@mycroftmind.com>
2021-05-04 13:22:17 -0700

    Fixed warnings.

--- src/book-search/BookSearch.tsx
+++ src/book-search/BookSearch.tsx
@@ -1,68 +1,69 @@
-import React, { useEffect, useState } from "react";
-import { getBooksByType } from "./book-search.service";
-
+import React, { useEffect, useState, useCallback } from 'react';
+import { getBooksByType } from './book-search.service';
 
 const BookSearch = () => {
-    const [bookType, updateBookType] = useState("");
-    const [bookTypeToSearch, updateBookTypeToSearch] = useState("");
-    const [allAvailableBooks, setAllAvailableBooks] = useState([]);
-    async function requestBooks() {
-        if (bookTypeToSearch) {
-            const allBooks = await getBooksByType(bookTypeToSearch);
-            setAllAvailableBooks(allBooks);
-        }
-    }
+	const [bookType, updateBookType] = useState('');
+	const [bookTypeToSearch, updateBookTypeToSearch] = useState('');
+	const [allAvailableBooks, setAllAvailableBooks] = useState([]);
 
-    useEffect(() => {
-        async function getAllBooks() {
-            await requestBooks();
-        }
-        getAllBooks();
-    }, [bookTypeToSearch]);
-    return (
-            <>
-                <div className="book--container">
-                    <div className="search-params">
-                        <div>
-                            <form
-                                onSubmit={(e) => {
-                                    debugger;
-                                    e.preventDefault();
-                                   updateBookTypeToSearch(bookType)
-                                }}
-                            >
-                                <input
-                                    className="full-width"
-                                    autoFocus
-                                    name="gsearch"
-                                    type="search"
-                                    value={bookType}
-                                    placeholder="Search for books to add to your reading list and press Enter"
-                                    onChange={e => updateBookType(e.target.value)}
-                                />
-                            </form>
-                            {!bookType && (
-                                <div className="empty">
-                                    <p>
-                                        Try searching for a topic, for example
-                                        <a onClick={() => {
-                                                updateBookType("Javascript");
-                                            }}
-                                        >
-                                            {" "}
-                                            "Javascript"
-                                        </a>
-                                    </p>
-                                </div>
-                            )}
+	const requestBooks = useCallback(async () => {
+		if (bookTypeToSearch) {
+			const allBooks = await getBooksByType(bookTypeToSearch);
+			setAllAvailableBooks(allBooks);
+		}
+	}, [bookTypeToSearch]);
 
-                        </div>
-                    </div>
-                </div>
-                {                <pre>{JSON.stringify(allAvailableBooks, null, 4)}</pre>
-                }
-            </>
-    );
+	useEffect(() => {
+		async function getAllBooks() {
+			await requestBooks();
+		}
+		getAllBooks();
+	}, [bookTypeToSearch, requestBooks]);
+
+	return (
+		<>
+			<div className="book--container">
+				<div className="search-params">
+					<div>
+						<form
+							onSubmit={(e) => {
+								debugger;
+								e.preventDefault();
+								updateBookTypeToSearch(bookType);
+							}}
+						>
+							<input
+								className="full-width"
+								autoFocus
+								name="gsearch"
+								type="search"
+								value={bookType}
+								placeholder="Search for books to add to your reading list and press Enter"
+								onChange={(e) => updateBookType(e.target.value)}
+							/>
+						</form>
+						{!bookType && (
+							<div className="empty">
+								<p>
+									Try searching for a topic, for example
+									<button
+										className="link-button"
+										onClick={() => {
+											updateBookType('Javascript');
+										}}
+									>
+										{' '}
+										"Javascript"
+									</button>
+								</p>
+							</div>
+						)}
+					</div>
+				</div>
+			</div>
+			{<pre>{JSON.stringify(allAvailableBooks, null, 4)}</pre>}
+		</>
+	);
 };
 
 export default BookSearch;
