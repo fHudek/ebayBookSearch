fb00ab9004248b63bfaf1c063a4bce34d1a02e5a
fero <fero@mycroftmind.com>
2021-05-06 09:51:50 -0700

    Added book count to wish list header.

--- src/components/wishList/WishListHeader.tsx
+++ src/components/wishList/WishListHeader.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { getWishListBookCount } from '../../features/books/selectors';
 
 const WishListHeader = () => {
-	return <h2 className="wish-list-header">My Wishlist</h2>;
+	const wishListBookCount = useSelector(getWishListBookCount);
+	return (
+		<h2 className="wish-list-header">{`My Wishlist (${wishListBookCount})`}</h2>
+	);
 };
 
 export default WishListHeader;
--- src/features/books/selectors.ts
+++ src/features/books/selectors.ts
@@ -5,6 +5,9 @@ export const getSearchListBooks = (state: RootState) => state.books.searchList;
 export const getWishListBooks = (state: RootState) =>
 	Object.values(state.books.wishList);
 
+export const getWishListBookCount = (state: RootState) =>
+	Object.keys(state.books.wishList).length;
+
 export const getWishListBookIds = (state: RootState) =>
 	Object.keys(state.books.wishList);
 
